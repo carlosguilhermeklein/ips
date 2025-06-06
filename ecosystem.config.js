764f0060ebcc2c0d3bddc640b718dead48488770
carlosguilhermeklein <84053199+carlosguilhermeklein@users.noreply.github.com>
2025-06-06 16:30:20 -0300

    Criar arquivo de configuração do PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [{
+    name: 'ip-manager',
+    script: './server/index.js',
+    cwd: '/var/www/html/ips',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'production',
+      PORT: 3001
+    },
+    error_file: '/var/log/pm2/ip-manager-error.log',
+    out_file: '/var/log/pm2/ip-manager-out.log',
+    log_file: '/var/log/pm2/ip-manager.log'
+  }]
+};
